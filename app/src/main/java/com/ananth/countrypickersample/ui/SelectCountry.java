2743918b1ee3c7e5540d1bcbae17343c943ac16d
Babu <atom.ananthababu@gmail.com>
2016-07-04 11:50:38 +0530

    some changes

---
+++ app/src/main/java/com/ananth/countrypickersample/ui/SelectCountry.java
@@ -0,0 +1,16 @@
+package com.ananth.countrypickersample.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.ananth.countrypickersample.R;
+
+public class SelectCountry extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_country);
+
+    }
+
+
+}
